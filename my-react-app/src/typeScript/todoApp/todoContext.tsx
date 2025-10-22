ddfd0cd79156c0e32a5503fb355a713b83c5bb53
mamad <sara.avaznjd@gmail.com>
2025-10-22 06:51:34 -0700

    Todo app with save todos in localStorage

--- my-react-app/src/typeScript/todoApp/todoContext.tsx
+++ my-react-app/src/typeScript/todoApp/todoContext.tsx
@@ -1,18 +1,18 @@
-import { createContext, useCallback, useMemo, useReducer } from "react";
+import { createContext, useCallback, useEffect, useMemo, useReducer } from "react";
 import type { Action, TodoContextType, TodoState } from "./type.js";
 
 
 const todoReducer = (state: TodoState, action: Action) => {
     switch (action.type) {
         case "ADD":
-            return [...state, {id:Date.now(), text: action.payload, completed: false}]
-    
+            return [...state, { id: Date.now(), text: action.payload, completed: false }]
+
         case "REMOVE":
             return state.filter(todo => todo.id !== action.payload)
-            
+
         case "TOGGLE":
             return state.map(todo => {
-                if(todo.id === action.payload) return {...todo, completed: !todo.completed}
+                if (todo.id === action.payload) return { ...todo, completed: !todo.completed }
                 else return todo
             })
         default:
@@ -20,16 +20,21 @@ const todoReducer = (state: TodoState, action: Action) => {
     }
 }
 
-export const todoContext = createContext<TodoContextType|undefined>(undefined)
+export const todoContext = createContext<TodoContextType | undefined>(undefined)
 
-export const TodoProvider: React.FC<{children: React.ReactNode}> = ({children}) => {
-    const [todos, dispatch] = useReducer(todoReducer,[])
+export const TodoProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    const initialState: TodoState = JSON.parse(localStorage.getItem('todos') || '[]')
+    const [todos, dispatch] = useReducer(todoReducer, initialState)
 
-    const addTodo = useCallback((text:string) => {dispatch({type:"ADD",payload:text})},[])
-    const toggleTodo = useCallback((id: number) => {dispatch({type:"TOGGLE",payload:id})},[])
-    const removeTodo = useCallback((id: number) => {dispatch({type:"REMOVE",payload:id})},[])
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos))
+    }, [todos])
 
-    const value = useMemo(() => ({todos,addTodo,toggleTodo,removeTodo}),[todos])
+    const addTodo = useCallback((text: string) => { dispatch({ type: "ADD", payload: text }) }, [])
+    const toggleTodo = useCallback((id: number) => { dispatch({ type: "TOGGLE", payload: id }) }, [])
+    const removeTodo = useCallback((id: number) => { dispatch({ type: "REMOVE", payload: id }) }, [])
+
+    const value = useMemo(() => ({ todos, addTodo, toggleTodo, removeTodo }), [todos])
 
     return <todoContext.Provider value={value}>{children}</todoContext.Provider>
 }
